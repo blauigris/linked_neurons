d8e35d0b6fe367e4aa07774b6c6a62fc9279ebdd
Carles R. Riera <blauigris@gmail.com>
2017-12-08 02:39:34 +0100

    Add missing requirements to setup.py

--- setup.py
+++ setup.py
@@ -12,17 +12,16 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    # TODO: put package requirements here
+    'keras >= 2.0',
+    'tensorflow >= 1.4'
 ]
 
 setup_requirements = [
     'pytest-runner',
-    # TODO(blauigris): put setup requirements (distutils extensions, etc.) here
 ]
 
 test_requirements = [
     'pytest',
-    # TODO: put package test requirements here
 ]
 
 setup(
